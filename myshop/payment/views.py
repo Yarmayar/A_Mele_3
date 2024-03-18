03901e89d8188869fd09dfe806f8a89aa20d2bab
Yarmayar <my4and@gmail.com>
2024-03-18 18:53:51 +0300

    ---476---Отсылки к платежам Stripe в заказах 1.1 Редактируем orders/models Каждый платеж Stripe имеет уникальный идентификатор. Его можно использовать для того, чтобы связывать каждый заказ с соответствующим платежом Stripe. Мы добавим новое поле в модель Order приложения orders, чтобы иметь возможность ссылаться на связанный платеж по его ИД. Такой подход позволит связывать каждый заказ с соответствующей транзакцией Stripe. 1.2 Редактируем функцию stripe_webhook в файле views.py Благодаря этому изменению при получении уведомления от веб-перехватчика о завершенном сеансе оформления платежа ИД намерения платежа сохраняется в поле stripe_id объекта order. Теперь Мы успешно ссылаемся на платежи Stripe в заказах. Теперь можно добавлять идентификаторы платежей Stripe в список заказов на сайте администрирования 2.1 Редактируем orders/models.py 2.2 Редактируем orders/admin.py

--- myshop/payment/views.py
+++ myshop/payment/views.py
@@ -47,6 +47,7 @@ def payment_process(request):
 def payment_completed(request):
     return render(request, 'payment/completed.html')
 
+
 def payment_canceled(request):
     return render(request, 'payment/canceled.html')
 
--- myshop/payment/webhooks.py
+++ myshop/payment/webhooks.py
@@ -33,6 +33,8 @@ def stripe_webhook(request):
                 return HttpResponse(status=400)
             # пометить заказ как оплаченный
             order.paid = True
+            # сохранить ИД платежа Stripe
+            order.stripe_id = session.payment_intent
             order.save()
 
     return HttpResponse(status=200)
