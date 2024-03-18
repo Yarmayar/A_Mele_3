1a4890c3081a2939779f8af83ced96aecec4c668
Yarmayar <my4and@gmail.com>
2024-03-18 17:01:04 +0300

    Применение веб-перехватчиков для получения уведомлений о платежах 1. Создание конечной точки веб-перехватчика 1.1 settings -> STRIPE_WEBHOOK_SECRET 1.2 Создаем файл webhooks.py в payment 1.3 Редактируем urls в payment
    2. Тестирование уведомлений веб-перехватчиков
    Для тестирования веб-перехватчиков необходимо установить интерфейс командной строки Stripe (CLI)
    2.1 brew install stripe/stripe-cli/stripe
    2.2 stripe login -> переходим на сайт, сверяем код, подтверждаем доступ
    2.3 stripe listen --forward-to localhost:8000/payment/webhook/ для сообщения стрип, что хотим прослушивать сообщения на нашем локальном хосте, адрес и порт соответствуют url в payment.urls
    Теперь по ссылке https://dashboard.stripe.com/test/webhook наш хост должен быть в списке слушателей
    
    Запускаем docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:management
    и проверяем совершая покупку, в терминал с запущенным стрим должны приходить стутусы обработки платежа
    
    (С ВПН НЕ РАБОТАЕТ ОТПРАВКА СООБЩЕНИЙ, БЕЗ ВПН НЕ РАБОТАЕТ ПРОСЛУШКА СТРАЙП)
    Теперь заказы автоматически помечаются как оплаченные с помощью платежных уведомлений Stripe.

--- myshop/orders/tasks.py
+++ myshop/orders/tasks.py
@@ -3,6 +3,7 @@ from celery import shared_task
 from django.core.mail import send_mail
 from .models import Order
 
+
 @shared_task
 def order_created(order_id):
     order = Order.objects.get(id=order_id)
